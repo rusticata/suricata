2209a213aca228748b3dc87c5617ecbca33c0388
Pierre Chifflier <chifflier@wzdftpd.net>
2017-01-25 08:51:54 +0100

    Reorganize Rust layout, add common interface for Rust parsers
    Propose a new directory layout, and add the following features:
    - build all parsers in a single archive (.a) file, to prevent code
      duplication and conflicts
    - add a crate for Suricata runtime function wrappers, so all parsers can
      use them. This crate also declares the interface (RParser trait) that
      all parsers must implement
    
    Also, add the files to Makefile.am EXTRA_DIST so 'make dist' includes
    them.

---
+++ rust/suricata-rs/src/lib.rs
@@ -0,0 +1,58 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+
+extern crate suricata_interface;
+use suricata_interface::*;
+
+/// Declare all parsers
+///
+/// Since this crate will produce a library, declaring the parsers is enough to
+/// instanciate all functions.
+extern crate suricata_dns;
+use suricata_dns::*;
+
+extern crate suricata_nfs3;
+use suricata_nfs3::*;
+
+extern crate suricata_quic;
+use suricata_quic::*;
+
+extern crate suricata_smb;
+use suricata_smb::*;
+
+
+
+// Starting from here, all code is optional
+
+
+// Example code to store all parsers in a single HashMap
+enum AllParsers<'a> {
+    Quic(QuicParser<'a>),
+}
+
+lazy_static! {
+    static ref HASHMAP: HashMap<String, AllParsers<'static>> = {
+        let mut m = HashMap::new();
+        m.insert("quic".to_string(),AllParsers::Quic(QuicParser::new(b"Quic")));
+        m
+    };
+}
+
+/// This function can be used for ex, to register all parsers to C,
+/// or to initialize some runtime functions, etc.
+#[no_mangle]
+pub extern "C" fn suricata_rust_init() -> u32 {
+    // let mut m : HashMap<String,AllParsers> = HashMap::new();
+    // m.insert("quic".to_string(),AllParsers::Quic(QuicParser::new(b"Quic")));
+
+    for (name,parser) in HASHMAP.iter() {
+        match *parser {
+            AllParsers::Quic(ref q) => (),
+            // _ => (),
+        }
+    }
+
+    0
+}
